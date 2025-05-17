d635f7f72109bca8df70ade7a4da2876a0e481a7
Musika Iqfat <musfikaiqfatmomo21.com>
2025-05-17 19:02:25 +0600

    sent and get data from the db

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const cors = require('cors');
+const app = express();
 const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors())
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.upkmpyu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.upkmpyu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // data Collection in the db
+    const coffeeCollection = client.db('coffeeDB').collection('coffees')
+
+    // get that data in the server site url
+    app.get('/coffees', async(req, res) =>{
+      const result = await coffeeCollection.find().toArray();
+      res.send(result);
+    })
+
+    // post or send data to the database
+    app.post('/coffees', async(req,res) =>{
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
   res.send('coffee store server in running')
