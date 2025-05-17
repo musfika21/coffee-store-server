cba9a6d4f44ceda60cb8769f9328bed8150826fb
Musika Iqfat <musfikaiqfatmomo21.com>
2025-05-17 22:23:11 +0600

    another commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
 const app = express();
@@ -34,11 +34,27 @@ async function run() {
       res.send(result);
     })
 
+    // single coffee details get 
+    app.get('/coffees/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    })
+
     // post or send data to the database
     app.post('/coffees', async(req,res) =>{
       const newCoffee = req.body;
       console.log(newCoffee);
       const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+    // delete data from db
+    app.delete('/coffees/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await coffeeCollection.deleteOne(query);
       res.send(result);
     })
 
